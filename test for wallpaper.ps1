3dda9a0ccd1901719c6ee3a1cdf442bfec10e9e4
Jason Dallas <146230423+daljas@users.noreply.github.com>
2023-11-17 11:20:58 -0500

    Update test for wallpaper.ps1

--- test for wallpaper.ps1
+++ test for wallpaper.ps1
@@ -8,13 +8,11 @@ $imagePath = "$env:TEMP\avalonlake-desktop.png"
 Invoke-WebRequest -Uri $imageUrl -OutFile $imagePath
 
 # Set the image as the desktop background for all users
-Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"
-Add-Type -TypeDefinition @'
+Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"Add-Type -TypeDefinition @"
     using System;
     using System.Runtime.InteropServices;
     public class Wallpaper {
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        [DllImport('user32.dll', CharSet = CharSet.Auto)]
         public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
     }
-'@; [Wallpaper]::SystemParametersInfo(20, 0, '$imagePath', 3)
-`""
+    [Wallpaper]::SystemParametersInfo(20, 0, '$imagePath', 3)
