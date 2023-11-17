4e073b867c419fe2688215741746b0786ad356ac
Jason Dallas <146230423+daljas@users.noreply.github.com>
2023-11-17 11:38:24 -0500

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -8,14 +8,3 @@ RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters, 1, True
 $newUserBackgroundPath = "https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/avalonlake-desktop.png"
 $registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI"
 Set-ItemProperty -Path $registryPath -Name 'Background' -Value $newUserBackgroundPath
-
-# Function to refresh the desktop
-function Refresh-Desktop {
-    param([Parameter(Position=0, Mandatory=$true)] [string]$Path)
-
-    $explorer = New-Object -ComObject shell.application
-    $explorer.NameSpace($Path).ParseName('desktop.ini').InvokeVerb('install')
-}
-
-# Refresh the desktop for new users
-Refresh-Desktop -Path "$env:SystemRoot\system32\config\systemprofile\Desktop"
