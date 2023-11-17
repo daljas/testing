9de219e972ad0a69e862d4bc1d69f449420b763e
Jason Dallas <146230423+daljas@users.noreply.github.com>
2023-11-17 11:18:58 -0500

    Create test for wallpaper.ps1

---
+++ test for wallpaper.ps1
@@ -0,0 +1,20 @@
+# URL of the image
+$imageUrl = "https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/avalonlake-desktop.png"
+
+# Path to save the image
+$imagePath = "$env:TEMP\avalonlake-desktop.png"
+
+# Download the image
+Invoke-WebRequest -Uri $imageUrl -OutFile $imagePath
+
+# Set the image as the desktop background for all users
+Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"
+Add-Type -TypeDefinition @'
+    using System;
+    using System.Runtime.InteropServices;
+    public class Wallpaper {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+    }
+'@; [Wallpaper]::SystemParametersInfo(20, 0, '$imagePath', 3)
+`""
