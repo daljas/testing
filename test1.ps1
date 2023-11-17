e79fb5acf8458f6aa749b8d9033147c8d5b65b88
Jason Dallas <146230423+daljas@users.noreply.github.com>
2023-11-17 11:35:39 -0500

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -1,12 +1,19 @@
-# Specify the path to the background image
-$backgroundImagePath = "https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/avalonlake-desktop.png"
+# Set the desktop background for the current user
+$currentUserBackgroundPath = "https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/avalonlake-desktop.png"
+SystemParametersInfo(20, 0, $currentUserBackgroundPath, 3)
 
-# Set the background for the current user
-SystemParametersInfo 0x0014, 0, $backgroundImagePath, 0
-# Force an immediate update
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+# Set the default desktop background for new users
+$newUserBackgroundPath = "https://raw.githubusercontent.com/avalon-lake-tech/mrbeast-scripts/main/avalonlake-desktop.png"
+$registryPath = "HKU\.DEFAULT\Control Panel\Desktop"
+New-ItemProperty -Path $registryPath -Name Wallpaper -Value $newUserBackgroundPath -PropertyType String
+SystemParametersInfo(20, 0, $newUserBackgroundPath, 3)
 
-# Set the background for the default user profile (new users)
-$defaultUserProfilePath = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders' -Name 'Common Desktop' | Select-Object -ExpandProperty 'Common Desktop'
-$defaultUserBackgroundPath = Join-Path $defaultUserProfilePath "background.jpg"
-Copy-Item -Path $backgroundImagePath -Destination $defaultUserBackgroundPath
+# Function to call SystemParametersInfo
+function SystemParametersInfo($uiAction, $uiParam, $pvParam, $fWinIni) {
+    $signature = @"
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    public static extern bool SystemParametersInfo(int uiAction, int uiParam, string pvParam, int fWinIni);
+"@
+    $type = Add-Type -MemberDefinition $signature -Name SystemParametersInfo -Namespace User32 -PassThru
+    $type::SystemParametersInfo($uiAction, $uiParam, $pvParam, $fWinIni) | Out-Null
+}
