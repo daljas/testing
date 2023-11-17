262587725cfa6316353a267050a9f270cd528ddc
Jason Dallas <146230423+daljas@users.noreply.github.com>
2023-11-17 11:28:47 -0500

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -1,36 +1,12 @@
-# Define URLs for Slack, Zoom, and AnyDesk
-$slackUrl = "https://codefellows.slack.com/archives/C065724314Y/p1700183701218439"
-$zoomUrl = "https://zoom.us/client/5.16.6.24712/ZoomInstallerFull.exe?archType=x64"
-$anyDeskUrl = "https://anydesk.com/en/downloads/thank-you?dv=win_exe"
+# Specify the path to the background image
+$backgroundImagePath = "C:\Path\To\Your\Image.jpg"
 
-# Define download paths
-$slackPath = "$env:USERPROFILE\Downloads\SlackSetup.exe"
-$zoomPath = "$env:USERPROFILE\Downloads\ZoomInstaller.exe"
-$anyDeskPath = "$env:USERPROFILE\Downloads\AnyDesk.exe"
+# Set the background for the current user
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name Wallpaper -Value $backgroundImagePath
+# Force an immediate update
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 
-# Function to download and install an application
-function DownloadAndInstall {
-    param (
-        [string]$Url,
-        [string]$Path
-    )
-
-    # Download the application
-    Invoke-WebRequest -Uri $Url -OutFile $Path -UseBasicParsing
-
-    # Install the application
-    Start-Process -Wait -FilePath $Path
-}
-
-# Start jobs for downloading and installing each application
-Start-Job -ScriptBlock { DownloadAndInstall -Url $slackUrl -Path $slackPath }
-Start-Job -ScriptBlock { DownloadAndInstall -Url $zoomUrl -Path $zoomPath }
-Start-Job -ScriptBlock { DownloadAndInstall -Url $anyDeskUrl -Path $anyDeskPath }
-
-# Wait for all jobs to complete
-Get-Job | Wait-Job
-
-# Receive the job results (optional)
-Receive-Job -AutoRemoveJob
-
-Write-Host "Installations completed."
+# Set the background for the default user profile (new users)
+$defaultUserProfilePath = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders' -Name 'Common Desktop' | Select-Object -ExpandProperty 'Common Desktop'
+$defaultUserBackgroundPath = Join-Path $defaultUserProfilePath "background.jpg"
+Copy-Item -Path $backgroundImagePath -Destination $defaultUserBackgroundPath
